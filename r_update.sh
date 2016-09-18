d353dc505721200a744902d273f907508e4cb646
Chloe Kudryavtsev <chloe.kudryavtsev@gmail.com>
2016-09-18 16:19:21 -0400

    Better checks for zsh in r_update

--- r_update.sh
+++ r_update.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 # meant to be called by dotfiles, but can be used if you're just lazy
 
-# we need the zcompile built-in
-[ -z "$ZSH_VERSION" ] && exec zsh -f $0
+# if we're not running zsh, we want to exec into it
+# we need zsh for zcompile
+if [ -z "$ZSH_VERSION" ]
+then
+    z="$(grep /etc/shells -e zsh | head -1)"
+    if [ -z "$z" ]
+    then
+        echo "Could not find a usable zsh installation!"
+        exit 1
+    else
+        exec "$z" -f "$0"
+    fi
+fi
 
 zshd="$(dirname $0)"
 . "$zshd/functions/toasty-zsh"
