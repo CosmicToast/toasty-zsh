682be1a1e182e960928a2e8aa3c1be62a6c214f3
Chloe Kudryavtsev <chloe.kudryavtsev@gmail.com>
2017-03-14 22:34:16 -0400

    [examples] Remove compile-stuff example.
    I really need to figure this stuff out, it acts weird.
    I don't want to recommend it until then.

--- examples/source/compile-stuff.zsh
+++
@@ -1,8 +0,0 @@
-# this will re-compile all teh functions every time your shell starts.
-# you probably do NOT want this, and is just here as an example/reminder that compiling is a thing, and you might want to do it occasionally
-
-autoload toasty-zsh
-
-toasty-zsh -cl
-
-# vim: ft=zsh
